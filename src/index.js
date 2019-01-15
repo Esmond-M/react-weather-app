995506b7c7e3a6bc84c96f163170bc4c2cd26bc0
Esmond <esmondmccain@gmail.com>
2019-01-15 16:24:50 -0600

    Babel core error

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+const WeatherApp = () => (
+  <div>
+    <span>
+      <i className="fab fa-react" />
+    </span>
+    <span>Weather App</span>
+    <span>
+      <i className="wi wi-day-sunny" />
+    </span>
+  </div>
+);
+
+const ROOT_NODE = document.getElementById("root");
+ReactDOM.render(<WeatherApp />, ROOT_NODE);
